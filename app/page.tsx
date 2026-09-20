f62a85d1da511ec9245a4a6e32ef375d05e5d83e
ravoeee-droid <ravoeee@gmail.com>
2026-09-20 07:52:18 +0200

    Compose MPP Wave 2 homepage sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,25 @@ import { Hero } from "@/components/mpp/hero";
 import { OperatingSystem } from "@/components/mpp/operating-system";
 import { Services } from "@/components/mpp/services";
 import { GrowthDiagnostic } from "@/components/mpp/growth-diagnostic";
+import { Problem } from "@/components/mpp/problem";
+import { Founder } from "@/components/mpp/founder";
+import { Process } from "@/components/mpp/process";
+import { ProofTriad } from "@/components/mpp/proof-triad";
+import { MotionScenes } from "@/components/mpp/motion-scenes";
 
 export default function HomePage() {
   return (
     <main>
+      <MotionScenes />
       <SiteHeader />
       <Hero />
       <OperatingSystem />
       <Services />
       <GrowthDiagnostic />
+      <Problem />
+      <Founder />
+      <Process />
+      <ProofTriad />
     </main>
   );
 }
