f68b8896cd9e74c538ad8077eb29de1d62826195
ravoeee-droid <ravoeee@gmail.com>
2026-09-21 06:52:38 +0200

    Remove literal newline escape in imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { SiteHeader } from "@/components/mpp/site-header";
 import { Hero } from "@/components/mpp/hero";
-import { MissionStory } from "@/components/mpp/mission-story";\nimport { ImmediateTrust } from "@/components/mpp/immediate-trust";
+import { MissionStory } from "@/components/mpp/mission-story";
+import { ImmediateTrust } from "@/components/mpp/immediate-trust";
 import { OperatingSystem } from "@/components/mpp/operating-system";
 import { Services } from "@/components/mpp/services";
 import { GrowthDiagnostic } from "@/components/mpp/growth-diagnostic";
